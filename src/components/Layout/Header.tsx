e8e3d6e61fd701f684ae954c8266bff68ece299e
benni <alby_b_@hotmail.it>
2022-07-17 12:12:51 +0200

    some css added and Header.tsx modified

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -3,13 +3,14 @@ import React from "react";
 import mealsImage from "../../assets/meals.jpeg";
 // @ts-ignore
 import classes from "./Header.module.css";
+import HeaderCartButton from "./HeaderCartButton";
 
-const Header = ()=> {
+const Header = () => {
     return (
         <>
             <header className={classes.header}>
                 <h1>ReactMeals</h1>
-                <button>Cart</button>
+                <HeaderCartButton/>
             </header>
             <div className={classes['main-image']}>
                 <img src={mealsImage} alt="A table full of food!"/>
@@ -17,3 +18,5 @@ const Header = ()=> {
         </>
     )
 };
+
+export default Header;
